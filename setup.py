068569e308b11c95bf5d826175d35a6aef0e3862
hirata10 <hirata.10@osu.edu>
2024-02-15 17:53:29 -0500

    replaced old distutils with setuptools

--- setup.py
+++ setup.py
@@ -1,36 +1,26 @@
 # setup script for pyimcom_croutines
 
-from distutils.core import setup, Extension
 import os
 from os.path import exists
+from setuptools import Extension, setup
 
-if __name__ == "__main__":
+# get include directories
+dirs = [];
+if exists('IncludePaths.txt'):
+  with open('IncludePaths.txt') as f:
+    lines = f.readlines()
+    for line in lines:
+      dirs += [line.strip()]
+# C routines
+ec = []
 
-  # get include directories
-  dirs = [];
-  if exists('IncludePaths.txt'):
-    with open('IncludePaths.txt') as f:
-      lines = f.readlines()
-      for line in lines:
-        dirs += [line.strip()]
 
-  # C routines
-  ec = []
-  setup(name="pyimcom_croutines",
-    version="1",
-    ext_modules=[Extension('pyimcom_croutines',
-      sources = ['pyimcom_croutines.c'],
-      include_dirs=dirs,
-      extra_compile_args=ec
-    )]
-  )
-
-  # Python wrappers
-  setup(name='pyimcom_lakernel',
-      version='1',
-      py_modules=['pyimcom_lakernel'],
-  )
-  setup(name='pyimcom_interface',
-      version='1',
-      py_modules=['pyimcom_interface'],
-  )
+setup(name='furryparakeet',
+  version='2',
+  ext_modules=[Extension('pyimcom_croutines',
+    sources = ['pyimcom_croutines.c'],
+    include_dirs=dirs,
+    extra_compile_args=ec
+  )],
+  py_modules=['pyimcom_croutines', 'pyimcom_lakernel', 'pyimcom_interface']
+)
